c0eb778ba4b2f395562952457cec067a3d015034
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-12-22 21:20:40 +0300

    add flex

--- App.js
+++ App.js
@@ -7,27 +7,48 @@ function App() {
   }
 
   return (
-    <SafeAreaView>
-      <View style={style.container}>
-        <Text style={{color: 'purple', fontSize: 50}}>Hello World</Text>
+    <SafeAreaView style={style.container}>
+      <View style={style.upper_view_container}>
+        {/* <Text style={{color: 'purple', fontSize: 50}}>Hello World</Text> */}
+        <Text>Hello World</Text>
       </View>
-      <Button
+      {/* <Button
         title="Here press!"
         onPress={sayHello}
         disabled={false}
         color="#e6e6fa"
-      />
+      /> */}
+      <View style={style.bottom_view_container}>
+        <Text>Hello 2</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
   //bu kısım json objesi
+  // container: {
+  //   backgroundColor: "red",
+  //   margin: 20,
+  //   borderRadius: 8,
+  // }
   container: {
-    backgroundColor: "red",
-    margin: 20,
-    borderRadius: 8,
-  }
-})
+    flex: 1, 
+    flexDirection: 'row'
+  },
+  upper_view_container: {
+    flex: 1,
+    backgroundColor: 'purple',
+  },
+  bottom_view_container: {
+    flex: 2,
+    backgroundColor: 'yellow',
+  },
+});
 
 export default App;
+
+//not
+//stillendirme de web'deki gibi px yok direk rakam veriyoruz. : margin: 10,
+//en dıştakine flex vermezsek childlar da çalışmaz.
+//flex yoksa bulunduğu alan kadar yer kaplayacak.
