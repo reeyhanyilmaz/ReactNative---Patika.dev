92ce372b7a34587f20ac5526702fe1665cbfbbc2
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-12-25 14:36:07 +0300

    add notes

--- src/App.js
+++ src/App.js
@@ -65,3 +65,22 @@ export default App;
 //flex yoksa bulunduğu alan kadar yer kaplayacak.
 
 //TouchableOpacity custom button olarak kullanırız burada
+
+//FlatList ekranda kullanıcı ne kadar veri görecekse o kadarını basar. Cihazı fazla yormayı önler.
+//Veriyi tek tek yükler bir anda yükleyip yormayı önler.
+//FlatList data ve renderItem diye iki prop döner. data verilerimiz, renderItem her bir item'larımız.
+
+//Dimensions : Dimensions.get("window").heigth /4 : ekranın yüksekliğinin 4'de 1!i demek.
+//style'da height: Dimensions.get("window").heigth /4 şeklinde verebiliriz.
+
+//keyExtractor ={(item, index) => item.u_id.toString()}
+//keyExtractor : biz ekranda görmüyorsak verileri siler, ihtiyac duurmıunda ekrana tekrar basılır veriler, hangisini basacağını veya kaldıracağını belirtmek için kullanılır.
+//id string ise toString()'e ihtiyaç yok.
+
+//renderItem = ({item}) => <NewsCard news={item} />} renderItem'ı FlatList içinde bu şekilde verirsek 200 veri olursa 200 kere arrow func. çalışacak ama yukarda const renderNews = ({item}) => <NewsCard news={item} /> şeklinde tanımlayıp , rendderItem'a renderItem = {renderNews} verirsel 200 kere func çağırılır ama 200 kere çalışmaz func. daha verimlidir.
+
+
+//aynı şekilde keyExtractor'da func halinde çağırılabilir.
+
+
+//ScrolView component'i bize scrol özelliğini kullanmamıza olanak tanır. <FlatList ListHeaderComponent={ buraya <ScrollView horizontal showHorizontaşScroolIndıcator={false} /> bunu yazarsak yukarıda sticky şekilde kalmaz scroll comp. flatlist içinde olur.}
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,1 +1,3 @@
 export {default} from "./Card";
+
+//import ile card'dan be dönüyorsa onu çağırıyorsam onu index.js yakalar ve bana default döndürüyorsa onu çekeriz.
