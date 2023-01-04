e629c65e585c0f01bbbcae6965edb3b9d2008daf
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2023-01-04 19:52:00 +0300

    add life cycle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import Card from './components/Card';
+import React, {useEffect, useState} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+// import Card from './components/Card';
 
 function App() {
-  function sayHello() {
-    console.log('hello');
+  // function sayHello() {
+  //   console.log('hello');
+  // }
+
+  //lifeCycle
+  //useEffect((),[]) dependency'de hangi state'i takip etmesini istiyorsak onu yazarız, useEf. içine de state değişince ne olacağını yazarız.
+  //component state değişince hep render olur zaten ama biz useEf. içine sadce ne olmasını istiyorsak onu yazarız.
+  const [hello, setHello] = useState(false);
+  function updateFlag() {
+    setHello(!hello);
   }
+  const [number, setNumber] = useState(0);
+  useEffect(() => {
+    console.log('deneme');
+  }, [number]); //dependency boş ise state takip etmez 1 kere çalışır sadece.
+  console.log('render'); // bu kısım her seferinde çalışır.
 
   return (
     <SafeAreaView style={style.container}>
@@ -24,10 +37,27 @@ function App() {
       </View> */}
 
       {/* custom component part **************************** */}
-      <Card title="Reyhan" text="frontend and mobile developer" />
-      <Card title="Mabel Matiz" text="Antidepresan" />
+      {/* <Card title="Reyhan" text="frontend and mobile developer" />
+      <Card title="Mabel Matiz" text="Antidepresan" /> */}
+
+      {hello && <Hello />}
+      <Button title="hello" onPress={updateFlag} />
+      {/* varsa göster yoksa gizle demek */}
     </SafeAreaView>
   );
+}
+
+function Hello() {
+  useEffect(() => {
+    console.log('1 kere çalışır');
+
+    //cleanUp function
+    return () => {
+      console.log('silindiği anda çalışır');
+    };
+  }, []);
+
+  return <Text>deneme</Text>;
 }
 
 const style = StyleSheet.create({
@@ -79,8 +109,6 @@ export default App;
 
 //renderItem = ({item}) => <NewsCard news={item} />} renderItem'ı FlatList içinde bu şekilde verirsek 200 veri olursa 200 kere arrow func. çalışacak ama yukarda const renderNews = ({item}) => <NewsCard news={item} /> şeklinde tanımlayıp , rendderItem'a renderItem = {renderNews} verirsel 200 kere func çağırılır ama 200 kere çalışmaz func. daha verimlidir.
 
-
 //aynı şekilde keyExtractor'da func halinde çağırılabilir.
-
 
 //ScrolView component'i bize scrol özelliğini kullanmamıza olanak tanır. <FlatList ListHeaderComponent={ buraya <ScrollView horizontal showHorizontaşScroolIndıcator={false} /> bunu yazarsak yukarıda sticky şekilde kalmaz scroll comp. flatlist içinde olur.}
